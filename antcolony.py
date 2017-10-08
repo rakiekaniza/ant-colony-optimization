f7abcd1ac0e0994ec52787ac09ea74138afc6a22
Raki Anwar Ekaniza <raki.anwar.1994@gmail.com>
2017-10-08 15:00:21 +0700

    First commit

---
+++ antcolony.py
@@ -0,0 +1,119 @@
+import numpy as np
+import random
+import math
+import sys
+import time
+
+class AncitiesColony:
+    rho = 0.8
+    alpha = 2
+    beta = 2
+    Q = 5
+            
+    s = 0
+    nc = 1
+    def __init__(self, data, ncities, nants, ncmax):
+        self.data = data
+            
+        self.ncmax = ncmax
+        self.nants = nants+1
+        self.ncities = ncities
+        
+        self.ants = np.empty((self.nants, 1), dtype=np.int32)
+        self.lengthk = np.zeros((self.nants, 1))
+        self.tabulist = np.zeros((self.nants, ncities), dtype=np.int32) 
+        self.pheromones = np.zeros((self.ncities, self.ncities))
+        self.pheromones.fill(1)
+        self.deltapheromones = np.zeros((self.ncities, self.ncities))
+    
+        self.shortestlength = sys.maxsize
+        self.shortestroute = np.zeros((ncities), dtype = np.int32)
+        
+    def distance(self, a, b):
+        return math.sqrt(pow(self.data[a][1]-self.data[b][1], 2)+pow(self.data[a][2]-self.data[b][2], 2))
+    
+    def deltapheromonesk(self, i, j, k):
+        useij = False
+        for x in range(0, self.ncities-1): 
+            if((self.tabulist[k][x] == i or self.tabulist[k][x] == j)
+               and ((self.tabulist[k][x-1] == i or self.tabulist[k][x-1] == j)
+               or (self.tabulist[k][x+1] == i or self.tabulist[k][x+1] == j))): useij = True
+        return self.Q/self.calculatelength(k) if useij else 0
+    
+    def calculatelength(self, k):
+        Lk = 0
+        for x in range(0, self.ncities-2): 
+            Lk += self.distance(self.tabulist[k][x]-1, self.tabulist[k][x+1]-1)
+        Lk += self.distance(self.tabulist[k][0]-1, self.tabulist[k][self.ncities-1]-1)
+        return Lk
+    
+    def findnewcity(self, i, citiestovisit):
+        divisor = 0
+        dividend = np.zeros((len(citiestovisit)))
+        for x in range(0, len(citiestovisit)):
+            np.put(dividend, x, pow(self.pheromones[i][citiestovisit[x]-1], self.alpha)*pow((1/self.distance(i, citiestovisit[x]-1)), self.beta))
+            divisor += pow(self.pheromones[i][citiestovisit[x]-1], self.alpha)*pow((1/self.distance(i, citiestovisit[x]-1)), self.beta)
+        dividend = dividend/divisor
+        return citiestovisit[self.findnearest(dividend, random.random())]
+    
+    def findnearest(self, citiestovisit, randnumber):
+        return (np.abs(citiestovisit-randnumber)).argmin()
+    
+    def antsroute(self, k, firstcity):
+        citiestovisit = [n for n in range(1, self.ncities+1) if n != firstcity]
+        visited = [firstcity]
+        s = 0
+        np.put(self.tabulist[k], [0], self.ants[k][0])
+        for i in range(1, len(citiestovisit)):
+            s += 1
+            newcity = self.findnewcity(visited[i-1]-1, citiestovisit)
+            visited.append(newcity)
+            citiestovisit.remove(newcity)
+        visited.append(citiestovisit[0])
+        return visited
+    
+    def searchroute(self):
+        for k in range (0, self.nants):
+            np.put(self.ants[k], [0], random.randint(1, self.ncities))
+            
+        doloop = True
+        
+        while doloop:
+            for k in range(0, self.nants):
+                self.tabulist[k] = self.antsroute(k, self.ants[k][0])
+            for k in range(0, self.nants):
+                np.put(self.lengthk[k], [0], self.calculatelength(k-1))
+                if(self.shortestlength>self.lengthk[k]):
+                    self.shortestlength = self.lengthk[k]
+                    self.shortestroute = self.tabulist[k]
+            for k in range (0, self.nants): 
+                deltapheromonesij = 0
+                for i in range(1, self.ncities):
+                    deltapheromonesij = self.deltapheromones[i-1][i]
+                    deltapheromonesij += self.deltapheromonesk(i, i+1, k)
+                    np.put(self.deltapheromones[i-1], [i], deltapheromonesij)
+                    np.put(self.deltapheromones[i], [i-1], deltapheromonesij)
+                    
+            self.pheromones = self.rho*self.pheromones + self.deltapheromones
+                    
+            self.nc += 1
+            
+            self.deltapheromones = np.zeros((self.ncities, self.ncities))
+            
+            if (self.nc < self.ncmax):
+                self.tabulist = np.zeros((self.nants, self.ncities), dtype=np.int32) 
+            else:
+                print('Shortest route \t\t= ', self.shortestroute)
+                print('Length of the route \t= ', self.shortestlength)
+                print("--- %s seconds ---" % (time.time() - start_time))
+                doloop = False
+
+if __name__ == '__main__':
+    start_time = time.time()
+    
+    np.set_printoptions(threshold=np.nan)
+    
+    data = np.loadtxt('Swarm052.txt')
+    
+    aco = AncitiesColony(data, len(data), 5, 5)
+    aco.searchroute()
