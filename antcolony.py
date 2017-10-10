405130a8099595197d512152348f5df4338a5f32
Raki Anwar Ekaniza <raki.anwar.1994@gmail.com>
2017-10-10 11:45:01 +0700

    Add comments

--- antcolony.py
+++ antcolony.py
@@ -4,13 +4,17 @@ import math
 import sys
 
 class AntColony:
-    rho = 0.8
+    #rho = penguapan, alpha = pangkat dari intensitas phereomone dalam probabilitas
+    #beta = pangkat dari visibilitas dalam probabilitas, Q = konstanta
+    rho = 0.7
     alpha = 2
-    beta = 2
+    beta = 3
     Q = 1
             
     s = 0
     nc = 0
+    
+    #inisialisasi matriks tabulist, pheromones, delta pheromones, dan variabel-variabel yang akan digunakan
     def __init__(self, data, nants, ncmax):
         np.set_printoptions(threshold=np.nan)
         
@@ -30,9 +34,11 @@ class AntColony:
         self.shortestlength = sys.maxsize
         self.shortestroute = np.zeros((self.ncities), dtype = np.int32)
         
+    #menghitung jarak dari kota a ke kota b
     def distance(self, a, b):
         return math.sqrt(pow(self.data[a][1]-self.data[b][1], 2)+pow(self.data[a][2]-self.data[b][2], 2))
     
+    #mengitung delta pheromones jalur dari kota i ke kota j yang dilalui oleh semut k
     def deltapheromonesk(self, i, j, k):
         useij = False
         for x in range(0, self.ncities-1): 
@@ -41,6 +47,7 @@ class AntColony:
                or (self.tabulist[k][x+1] == i or self.tabulist[k][x+1] == j))): useij = True
         return self.Q/self.calculatelength(k) if useij else 0
     
+    #menghitung panjang rute semut k
     def calculatelength(self, k):
         Lk = 0
         for x in range(0, self.ncities-2): 
@@ -48,57 +55,72 @@ class AntColony:
         Lk += self.distance(self.tabulist[k][0]-1, self.tabulist[k][self.ncities-1]-1)
         return Lk
     
+    #mencari kota baru menggunakan rumus probabilitas
     def findnewcity(self, i, citiestovisit):
         divisor = 0
         dividend = np.zeros((len(citiestovisit)))
         cumulativeprob = 0
+        #menghitung probabilitas dari setiap kota yang bisa dilaui
         for x in range(0, len(citiestovisit)):
             cumulativeprob += pow(self.pheromones[i][citiestovisit[x]-1], self.alpha)*pow((1/self.distance(i, citiestovisit[x]-1)), self.beta)
             np.put(dividend, x, cumulativeprob)
             divisor += pow(self.pheromones[i][citiestovisit[x]-1], self.alpha)*pow((1/self.distance(i, citiestovisit[x]-1)), self.beta)
         dividend = dividend/divisor
+        #menggunakan teknik roulette wheel selection
         randnumber = random.random()
-        return citiestovisit[(np.abs(dividend-randnumber)).argmin()]
+        return citiestovisit[np.searchsorted(dividend, randnumber)]
     
+    #menhasilkan rute yang dilallui oleh semut k
     def antsroute(self, k, firstcity):
+        #list kota yang harus dilalui oleh semut k
         citiestovisit = [n for n in range(1, self.ncities+1) if n != firstcity]
+        #rute yang sudah dilalui oleh semut k
         visited = [firstcity]
-        s = 0
-        np.put(self.tabulist[k], [0], self.ants[k][0])
         for i in range(1, len(citiestovisit)):
-            s += 1
             newcity = self.findnewcity(visited[i-1]-1, citiestovisit)
+            #menambahkan kota yang dihasilkan oleh fungsi findnewcity ke rute
             visited.append(newcity)
+            #menghapus kota yang sudah dilalui dari list kota yang akan dilalui
             citiestovisit.remove(newcity)
         visited.append(citiestovisit[0])
         return visited
     
+    #menggunakan 
     def searchroute(self):
+        #meletakkan semut k dikota awal
         for k in range (0, self.nants):
             np.put(self.ants[k], [0], random.randint(1, self.ncities))
             
         doloop = True
         
         while doloop:
+            #menggunakan fungsi antsroute untuk mengisi tabulist
             for k in range(0, self.nants):
                 self.tabulist[k] = self.antsroute(k, self.ants[k][0])
+                
+            #mencari rute terpendek yang dilalui oleh semua semut
             for k in range(0, self.nants):
                 np.put(self.lengthk[k], [0], self.calculatelength(k-1))
                 if(self.shortestlength>self.lengthk[k]):
                     self.shortestlength = self.lengthk[k]
                     self.shortestroute = self.tabulist[k]
+                
+            #menghitung delta peheromone dari setiap jalur
             for k in range (0, self.nants): 
                 deltapheromonesij = 0
                 for i in range(1, self.ncities):
                     deltapheromonesij = self.deltapheromones[i-1][i]
                     deltapheromonesij += self.deltapheromonesk(i, i+1, k)
+                    #karena jalur/graph tidak berarah, maka update jalur dilakukan di dua cell
                     np.put(self.deltapheromones[i-1], [i], deltapheromonesij)
                     np.put(self.deltapheromones[i], [i-1], deltapheromonesij)
                     
+            #update pheromone
             self.pheromones = self.rho*self.pheromones + self.deltapheromones
                     
             self.nc += 1
             
+            #mengosongkan delta pheromone
             self.deltapheromones = np.zeros((self.ncities, self.ncities))
             
             if (self.nc < self.ncmax):
@@ -107,3 +129,4 @@ class AntColony:
                 print('Shortest route \t\t= ', self.shortestroute)
                 print('Length of the route \t= ', self.shortestlength)
                 doloop = False
+#                return self.tabulist
--- tspsolver.py
+++ tspsolver.py
@@ -3,6 +3,6 @@ import time
 
 if __name__ == '__main__':
     start_time = time.time()
-    aco = antcolony.AntColony('data/Swarm202.txt', 20, 8)
+    aco = antcolony.AntColony('data/Swarm052.txt', 8, 10)
     aco.searchroute()
     print("--- %s seconds ---" % (time.time() - start_time))
